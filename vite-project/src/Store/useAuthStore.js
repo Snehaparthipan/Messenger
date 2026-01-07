1973ccd209d11695444b102b5b56ac21847475c6
Snehaparthipan <snehaparthipan123@gmail.com>
2026-01-07 16:29:53 +0000

    deploye frontend and backend

--- vite-project/src/Store/useAuthStore.js
+++ vite-project/src/Store/useAuthStore.js
@@ -2,8 +2,8 @@ import { create } from "zustand";
 import { axiosInstance } from "../Utills/axios";
 import toast from "react-hot-toast";
 import {io} from 'socket.io-client'
-// const BASE_URL = import.meta.env.VITE_SOCKET_URL;
-const BASE_URL="http://localhost:5000"
+const BASE_URL = import.meta.env.VITE_SOCKET_URL;
+// const BASE_URL="http://localhost:5000"
 export const useAuthStore=create((set,get)=>({
     authUser:null,
     isSignUp:false,

3b621a04a290918599087c03c66da9a02c580ad6
Snehaparthipan <snehaparthipan123@gmail.com>
2026-01-07 16:45:05 +0000

    change

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,10 @@ const PORT = process.env.PORT || 5000; // 👈 from .env
 app.use(express.json());
 app.use(
   cors({
-    origin:"https://messenger-n218.vercel.app",
+    origin: [
+      "http://localhost:5173",
+      "https://messenger-n218.vercel.app"
+    ],
     credentials: true,
   })
 );

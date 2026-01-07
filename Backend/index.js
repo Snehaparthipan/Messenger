0dbb6ba8bf9688f8cc8bab78f3fd7295bf986236
Snehaparthipan <snehaparthipan123@gmail.com>
2026-01-07 07:55:32 +0000

    deploye backend

--- Backend/index.js
+++ Backend/index.js
@@ -5,14 +5,14 @@ const connectDB=require("../Backend/src/Config/db")
 const authRoutes=require("../Backend/src/Routes/authRoutes")
 const messageRoutes=require("../Backend/src/Routes/messageRoutes")
 const {app,server}=require("./src/lib/socket")
-app.use("/api/auth",authRoutes)
-app.use("/api/message",messageRoutes)
 app.use(express.json())
 app.use(cors({
     origin:"https://messenger-n218.vercel.app",
     credentials:true
 }))
 app.use(cookiPearser())
+app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes)
 server.listen(5000,()=>{
     console.log("server run in 5000")
     connectDB()

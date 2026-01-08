f5542e705fb940aa2452196ab3170e726a15124f
Snehaparthipan <snehaparthipan123@gmail.com>
2026-01-08 07:34:15 +0000

    change

--- vite-project/src/Utills/axios.js
+++ vite-project/src/Utills/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL,
-baseURL:"https://messenger-2-yb82.onrender.com/api",
-  withCredentials: true
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "https://messenger-r42b.onrender.com//api",
+  withCredentials: true,
 })
+
